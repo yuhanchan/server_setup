0961b1c00ea4a0d1c227641cba2fe05fa5e5e59a
Yuhan Chen <chenyh@umich.edu>
2020-07-03 22:39:22 -0400

    update

--- setup.sh
+++ setup.sh
@@ -7,46 +7,13 @@ set -e
 sudo apt update
 sudo apt upgrade -y
 
-pkgs=(
-    git 
-    vim 
-    htop 
-    gcc 
-    g++ 
-    build-essential 
-    nasm 
-    yasm 
-    tar 
-    python 
-    python3
-    python-pip 
-    python3-pip 
-    zip unzip 
-    autoconf 
-    automake 
-    cmake 
-    git-core
-    libass-dev 
-    libfreetype6-dev 
-    libgnutls28-dev 
-    libsdl2-dev 
-    libtool 
-    libva-dev 
-    libvdpau-dev 
-    libvorbis-dev 
-    libxcb1-dev 
-    libxcb-shm0-dev 
-    libxcb-xfixes0-dev 
-    pkg-config 
-    texinfo 
-    wget 
-    zlib1g-dev 
-    libelf-dev 
-    libssl-dev
-)
-
 # install essential pkgs
-for pkg in ${pkgs[@]}; do
+for pkg in  git vim htop gcc g++ build-essential nasm yasm tar python python3 \
+            python-pip python3-pip zip unzip autoconf automake cmake git-core \
+            libass-dev libfreetype6-dev libgnutls28-dev libsdl2-dev libtool \
+            libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+            libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev libelf-dev \
+            libssl-dev; do
     sudo apt install -y $pkg
 done
 
